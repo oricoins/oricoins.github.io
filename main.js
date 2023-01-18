a50bc28ba73e7a322c9d9a851699cd0d921b6405
JS Animations <80853499+JS-Animatios@users.noreply.github.com>
2023-01-18 08:08:43 -0500

    Update main.js

---
+++ main.js
@@ -0,0 +1,27 @@
+
+var email = document.getElementById('email').value;
+var password = document.getElementById('psw').value;
+var loginbox = document.getElementById('loginbox');
+var hideonstart = document.getElementById('hideonstart');
+var loginbtn = document.getElementById('signinbutton');
+var psw = document.getElementById('psw');
+var loginusername = document.getElementById('email');
+var signinbutton = document.getElementById('signupbutton');
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+hideonstart.style.display="none";
+loginbtn.style.display="none";
+psw.style.display="none";
+loginusername.style.display="none";
+loginbtn.style.display="none";
+signinbutton.style.display="none";
+loginbox.style.height="500px";
+sleep(2000).then(() => {
+hideonstart.style.display="block";
+loginbtn.style.display="block";
+psw.style.display="block";
+loginusername.style.display="block";
+signinbutton.style.display="block";
+})
