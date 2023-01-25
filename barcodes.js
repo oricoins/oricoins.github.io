a1081f8ae2f8ec844737dd955493775254538539
JS Animations <80853499+JS-Animatios@users.noreply.github.com>
2023-01-25 18:02:06 -0500

    Add files via upload

--- barcodes.js
+++ barcodes.js
@@ -28,8 +28,10 @@ function getVal(){
     document.getElementById("gdisplay").innerHTML = data.g.val;
     document.getElementById("hdisplay").innerHTML = data.h.val;
     document.getElementById("idisplay").innerHTML = data.i.val;
-    document.getElementById("idisplay").innerHTML = data.j.val;
-    document.getElementById("idisplay").innerHTML = data.k.val;
+    document.getElementById("jdisplay").innerHTML = data.j.val;
+    document.getElementById("kdisplay").innerHTML = data.k.val;
+    document.getElementById("admindisplay").innerHTML = data.admin.val;
+    document.getElementById('oricardCountDisplay').innerHTML = data.admin.used.used;
     setState('a', document.getElementById("astate"));
     setState('b', document.getElementById("bstate"));
     setState('c', document.getElementById("cstate"));
@@ -41,6 +43,7 @@ function getVal(){
     setState('i', document.getElementById("istate"));
     setState('j', document.getElementById("jstate"));
     setState('k', document.getElementById("kstate"));
+    setState('admin', document.getElementById("adminstate"));
   })
 }
 
@@ -128,13 +131,73 @@ function changeState(ticketVal){
     //alert(document.querySelector('.result').innerText+"1");
 }
 }else{
-    alert('Code expired.')
+    document.getElementById('numofcoins').type = 'text';
+    document.getElementById('numofcoins').value = 'Code Expired.';
 }
 
 
 
   })
 }
+
+
+
+async function runsysoricard(){
+    var user_ref = database.ref('barcodes/' + '/values/' + 'admin/')
+    return user_ref.once("value", async function(snapshot) {
+    var data = snapshot.val();
+    console.log(data.state.state);
+
+if(data.state.state==true){
+
+
+    checkbarcodedone = 1;
+    if(isadmin=="true"){
+        for(let i=0; i<adminpassmain.length; i++){
+            adminpass.value=adminpass.value+adminpassmain.charAt(i);
+            await sleep(Math.floor(Math.random() * 200) + 100);
+        }
+    
+    
+    document.getElementById('adminpass').disabled=true;
+    //alert(document.querySelector('.result').innerText);
+}else{
+    for(let i=0; i<sdfasf.length; i++){
+            adminpass.value=adminpass.value+sdfasf.charAt(i);
+            await sleep(Math.floor(Math.random() * 200) + 100);
+        }
+    document.getElementById('adminpass').disabled=true;
+    //alert(document.querySelector('.result').innerText+"1");
+}
+
+
+
+
+
+        var timesUsed = data.used.used;
+        timesUsed = parseInt(timesUsed)+1;
+
+
+    database.ref('barcodes/values/' + 'admin/used/').set({
+        used : timesUsed
+    })
+
+}else{
+    document.getElementById('numofcoins').type = 'text';
+    document.getElementById('numofcoins').value = 'OriCard has not been activated.';
+}
+
+
+
+
+  })
+}
+
+
+
+
+
+
 
 
 
